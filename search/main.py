2a54fc7d8790f791d33ff000000c6ef22f2977dd
Elijah Nicol <elijah_nicol@hotmail.com>
2021-03-21 21:28:49 +1100

    Load in Tokens from JSON
    All tokens are loaded in as separate arrays - upperTokens, lowerTokens and blockTokens.
    
    Have also added a function that turns the data from the tokens into a format that can be printed by print_board()

--- search/main.py
+++ search/main.py
@@ -13,6 +13,19 @@ import json
 # inside the `search` directory (like this one and `util.py`) and
 # then import from them like this:
 from search.util import print_board, print_slide, print_swing
+from search.token import Token
+
+# Generates a board dictionary in the format of the board printing
+# function
+def generate_board_dict(uppers,lowers,blocks):
+    board_dict = {}
+    for upper in uppers:
+        board_dict[(upper.x,upper.y)] = f"{upper.type}, u"
+    for lower in lowers:
+        board_dict[(lower.x,lower.y)] = f"{lower.type}, l"
+    for block in blocks:
+        board_dict[(block.x,block.y)] = "block"
+    return board_dict
 
 def main():
     try:
@@ -28,3 +41,21 @@ def main():
     # Why not start by trying to print this configuration out using the
     # `print_board` helper function? (See the `util.py` source code for
     # usage information).
+    
+    # Load in upper, lower and block tokens as seperate arrays (There
+    # may be a more compact way to do this)
+    upperTokens = []
+    lowerTokens = []
+    blockTokens = []
+    for upperRaw in data['upper']:
+        upper = Token(upperRaw[1], upperRaw[2], upperRaw[0])
+        upperTokens.append(upper)
+    for lowerRaw in data['lower']:
+        lower = Token(lowerRaw[1], lowerRaw[2], lowerRaw[0])
+        lowerTokens.append(lower)
+    for blockRaw in data['block']:
+        block = Token(blockRaw[1], blockRaw[2], "b")
+        blockTokens.append(block)
+
+    # Print board to show off function
+    print_board(generate_board_dict(upperTokens,lowerTokens,blockTokens))
--- search/token.py
+++ search/token.py
@@ -1,9 +1,9 @@
 class Token:
-
-    def __init__(self,x,y):
+    def __init__(self,x,y,type):
         self.x = x
         self.y = y
         self.isAlive = True
+        self.type = type
 
     def placement(self):
         return (self.x,self.y)
@@ -17,6 +17,9 @@ class Token:
     def move(self,x,y):
         self.x = x
         self.y = y
+
+    def __str__(self):
+        return f'Token is at ({self.x},{self.y}) and is of type "{self.type}"'
     
 
 
