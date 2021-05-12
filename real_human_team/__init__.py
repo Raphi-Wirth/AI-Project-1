4914f3170443bae790888f800c5cacada5e2031f
Elijah Nicol <elijah_nicol@hotmail.com>
2021-05-12 16:09:43 +1000

    PLAYER FIVE
    Added probably the best bot to the game

--- real_human_team/__init__.py
+++ real_human_team/__init__.py
@@ -2,3 +2,4 @@ from real_human_team.player import Player
 from real_human_team.player2 import Player2
 from real_human_team.player3 import Player3
 from real_human_team.player4 import Player4
+from real_human_team.player5 import Player5
